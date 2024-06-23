89e30d7eb0d84e04952447f8ad2e599434fb5d05
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-23 19:44:46 +0100

    style: pycodestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,10 +2,12 @@
 """ simple type annotated function"""
 from typing import Callable
 
+
 def mult_res(mult: float) -> float:
+    """sample function"""
     return 2 * mult
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """multiplier function """
     return mult_res
-    
