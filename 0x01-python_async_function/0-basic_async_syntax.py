a47e5aa3e804de50f6671361fbf348fdda58ec3a
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-29 03:03:40 +0100

    create and run concurrent routines

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,4 +8,4 @@ async def wait_random(max_delay: int = 10) -> float:
     """returns an int after a delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return max_delay
+    return delay
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+""" execute multiple coroutines"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ generates n instances of  wait_random"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+
+    results = await asyncio.gather(*tasks)
+    return results
