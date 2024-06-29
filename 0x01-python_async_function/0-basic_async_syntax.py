c16ce8ecd97b1ea4bee8693018b022cfa611e983
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-29 02:31:51 +0100

    fix: random number is now float

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """returns an int after a delay"""
-    delay = random.randint(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return max_delay
