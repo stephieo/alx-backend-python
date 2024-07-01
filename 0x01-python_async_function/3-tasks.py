842cfe580c1a20820b4fabc4eb1d1d23fa83a493
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-01 14:07:33 +0100

    docs: correct return type hint

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ function to return """
 import asyncio
-from typing import Awaitable
+from asyncio import Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> Task:
     """ returns a task object"""
     return asyncio.create_task(wait_random(max_delay))
