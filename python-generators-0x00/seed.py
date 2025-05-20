2c779fb7ae825e3c5d58d2f17652a31d623bc7f0
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-05-20 11:30:10 +0100

    feat: functions to stream data from csv file into mysql database

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,8 +1,7 @@
-# from sqlalchemy import create_engine, Column, String, 
-# from sqlalchemy.dialects.mysql import UUID
-# from sqlalchemy.orm import declarative_base, sessionmaker
 import mysql.connector as msx
+import uuid
 import csv
+
 
 def connect_db():
     """ connects  to a a myssql databases server
@@ -15,13 +14,14 @@ def connect_db():
     except msx.Error as err:
         print("Error connecting to server")
 
-    return conn    
+    return conn
+
 
 def create_database(connection):
     """ creates the ALX_prodev database"""
     cursor = connection.cursor()
     try:
-        connection.execute(
+        cursor.execute(
             "CREATE DATABASE ALX_prodev"
         )
     except msx.Error as err:
@@ -29,7 +29,8 @@ def create_database(connection):
 
     cursor.close()
     connection.close()
-   
+
+
 def connect_to_prodev():
     """connects to the ALX_prodev database
     returns:
@@ -37,54 +38,62 @@ def connect_to_prodev():
     """
     try:
         conn = msx.connect(user='ife', password='deputy',
-                            host='localhost',
-                            database='ALX_prodev')
+                           host='localhost',
+                           database='ALX_prodev')
     except msx.Error as err:
         print("Error connecting to database")
+        exit(1)
+        conn.cursor().execute('USE ALX_prodev')
+        conn.cursor().close()
+    return conn
 
-    return conn 
 
 def create_table(connection):
     """creates a table 'user_data' if not existing"""
     cursor = connection.cursor()
     try:
-        connection.execute(
+        cursor.execute(
             '''CREATE TABLE user_data(
-                user_id UUID PRIMARY KEY,
+                user_id CHAR(36) PRIMARY KEY,
                 name VARCHAR(250) NOT NULL,
                 email VARCHAR(250) NOT NULL,
-                age DECIMAL NOT NULL
-                Index idx_user_id (user_id)
+                age DECIMAL NOT NULL,
+                INDEX idx_user_id (user_id)
                 )'''
         )
     except msx.Error as err:
         print(" Table already exists")
 
     cursor.close()
-    connection.close()
-    
+
+
 def read_csv_generator(csv_file_path):
     """ a generator that reads each line of a csv file"""
     with open(csv_file_path, 'r')as f:
-        for line in f:
-            yield line.strip()
+        datarow = csv.DictReader(f)
+        for row in datarow:
+            row['user_id'] = uuid.uuid4().hex
+
+            yield {
+                'user_id': row['user_id'],
+                'name': row['name'],
+                'email': row['email'],
+                'age': row['age']
+            }
+
 
 def insert_data(connection, data):
     """inserts data in the database if non-existent using  python generator"""
     cursor = connection.cursor()
-    print(next(read_csv_generator(data)))
-
-        
-
-
-connection = seed.connect_db()
-if connection:
-    seed.create_database(connection)
-    connection.close()
-    print(f"connection successful")
-
-    connection = seed.connect_to_prodev()
-
-    if connection:
-        seed.create_table(connection)
-        seed.insert_data(connection, 'user_data.csv')
+    for line in read_csv_generator(data):
+        try:
+            cursor.execute(
+                '''INSERT INTO user_data
+                (user_id, name, email, age)
+                VALUES (%s, %s, %s, %s)''',
+                (line['user_id'], line['name'], line['email'], line['age'])
+            )
+        except msx.Error as err:
+            raise err
+    connection.commit()
+    cursor.close()
