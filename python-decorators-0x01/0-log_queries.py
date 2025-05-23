6e78b9cffe8aa436ad12db3838644515cf03b210
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-05-24 00:00:15 +0100

    feat: use datetime module for logging

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -2,14 +2,29 @@ import sqlite3
 import functools
 import logging
 from os import sys 
+from datetime import datetime
 
-### setting up logging
+
+
+
 #### decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        ### setting up logger
+            #create custom logger
         logger = logging.getLogger(__name__)
-        logger.warning(f"Using query: {kwargs['query']}")
+        logger.setLevel("DEBUG")
+            #create handler and add to logger
+        console_handler = logging.StreamHandler()
+        logger.addHandler(console_handler)
+            # create formatter and add to handler
+        formatter = logging.Formatter(
+            '{levelname} - {message}',
+            style='{',
+        )
+        console_handler.setFormatter(formatter)
+        logger.debug(f"{datetime.now()} - Using query: {kwargs['query']}")
         value = func(*args, **kwargs)
         return value
     return wrapper
