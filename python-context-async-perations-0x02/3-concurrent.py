9dcff93042cc8810446aa404980672240d3b99df
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-05-25 20:39:08 +0100

    feat: add concurrent async user data retrieval

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,29 @@
+import asyncio
+import aiosqlite
+
+
+async def async_fetch_users():
+    async with aiosqlite.connect('users.db') as db:
+        # this allows me to access data by the column name
+        db.row_factory = aiosqlite.Row
+        async with db.execute("SELECT * FROM users")as cursor:
+            print("Retrieving all users")
+            async for row in cursor:
+                print(f"{row['user_id']}: {row['name']} - {row['age']}")
+
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect('users.db') as db:
+        db.row_factory = aiosqlite.Row
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            print("Retrieving users above 40")
+            async for row in cursor:
+                print(f"{row['user_id']}: {row['name']} - {row['age']}")
+
+
+async def fetch_concurrently():
+    fetch_tasks = asyncio.gather(async_fetch_users(), async_fetch_older_users())
+    await fetch_tasks
+
+# runs coroutines by running the entry point
+asyncio.run(fetch_concurrently())
