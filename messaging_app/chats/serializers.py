38ab0ba497569a590e52130663b08891f7a39ff3
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-06-06 13:51:26 +0100

    feat: include read-only display name in UserSerializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from .models import User, Message, Conversation
 
 class UserSerializer(serializers.ModelSerializer):
-    display_name = serializers.CharField()
+    display_name = serializers.CharField(read_only=True, source='get_full_name')
     class Meta:
         model = User
-        fields = ['user_id', 'email', 'first_name', 'phone_number' 'last_name']
+        fields = ['user_id', 'email',  'phone_number' 'last_name']
 # this  is a list of all the fields that should be (de)serialized( converted to/from JSON)
 # these are fields that should only be serialized i.e they won't work with POST or PUT requests 
 
