53074749992beeffae248253acec2d54d6a66a32
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-06-06 15:47:36 +0100

    feat: add SerializerMethodField to conversation serializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -7,6 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['user_id', 'email',  'phone_number' 'last_name']
 # this  is a list of all the fields that should be (de)serialized( converted to/from JSON)
+        read_only_fields = ['user_id']
 # these are fields that should only be serialized i.e they won't work with POST or PUT requests 
 
 
@@ -19,12 +20,15 @@ class MessageSerializer(serializers.ModelSerializer):
 
 
 class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    conversation_title = serializers.SerializerMethodField()
     messages = MessageSerializer(many=True, read_only=True) 
     # the many  means that yo're expecting more tha one message to be returned
     # the read-only means that messages are ntot required to create a conversation 
 
     class Meta:
         model = Conversation
-        fields = '__all__'
         exclude =['conversation_id']
-
+    
+    def get_conversation_title(self,obj):
+        return f"{obj.participants[0].get_short_name} and {obj.participants[1].get_short_name}"
