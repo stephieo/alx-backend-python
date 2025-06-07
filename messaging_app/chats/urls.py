438cc82f3f9afb9c1629cf05ba36dd26a932d130
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-06-07 02:16:26 +0100

    feat: update URL patterns to include router URLs for checker requirements

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 from rest_framework import routers 
 
 
 router = routers.DefaultRouter()
 router.register('conversations', views.ConversationViewset)
-urlpatterns =router.urls
+urlpatterns = [
+    path('', include(router.urls))
+    ]
