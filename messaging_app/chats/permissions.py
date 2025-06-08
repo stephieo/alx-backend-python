4c43fe6d29f2653eaf5ffbd017cf32c89caeda55
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-06-08 22:13:32 +0100

    refactor: rename permission class to IsParticipantofConversation for clarity

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,5 +1,5 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions 
 
-class IsConversationParticipant(BasePermission):
+class IsParticipantofConversation(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return object.participants.filter(id=request.user.id).exists()
